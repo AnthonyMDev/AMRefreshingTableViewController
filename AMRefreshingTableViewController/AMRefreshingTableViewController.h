06e11a3b2761550c0cb06f72ba90d5e86d8ea03c
Anthony Miller <anthony@app-order.com>
2014-09-10 22:20:06 -0700

    Added documentation

--- AMRefreshingTableViewController/AMRefreshingTableViewController.h
+++ AMRefreshingTableViewController/AMRefreshingTableViewController.h
@@ -31,24 +31,63 @@
 @protocol AMRefreshingTableViewControllerDelegate;
 @protocol AMRefreshingTableViewControllerDataSource;
 
+/**
+ * `AMRefreshingTableViewController` is a `UITableViewController` that allows you to display a list of items retrived from an external data source. This controller supports asynchronous data retrieval and has pull to refresh and infinite scrolling capabilities.
+ */
 @interface AMRefreshingTableViewController : UITableViewController <ALTableViewCellFactoryDelegate, AMPullToRefreshDelegate>
 
+/**
+ *  Initializes an instance of `AMRefreshingTableViewController` with a given data source and a number of list items to display per page.
+ *
+ *  @note The `listItemsPerPage` sets the number of items to request from the data source when loading new list items. For infinite scrolling, this is the number of new items the controller will expect to add to the table view.
+ *
+ *  @param dataSource       The data source for the controller
+ *  @param listItemsPerPage The number of list items to display per page
+ *
+ *  @return An instance of `AMRefreshingTableViewController`
+ */
 - (instancetype)initWithDataSource:(id <AMRefreshingTableViewControllerDataSource>)dataSource listItemsPerPage:(NSUInteger)listItemsPerPage;
 
+/**
+ *  This method load an additional page of list items and inserts them at the bottom of the table view. This method is called automatically when infinite scroll is activated.
+ */
 - (void)loadNextItems;
 
+/**
+ *  This method refreshes the entire list of items. This method is called automatically when pull to refresh is activated.
+ */
 - (void)refreshList;
 
-@property (strong, nonatomic) NSMutableArray *listItemsArray;
-
-@property (assign, nonatomic) NSUInteger lastPageLoaded;
-
+/**
+ *  The data source for the controller (The data source must implement the `AMRefreshingTableViewControllerDataSource` protocol.)
+ */
 @property (strong, nonatomic) id <AMRefreshingTableViewControllerDataSource> dataSource;
 
+/**
+ *  The delegate for the controller (The delegate must implement the `AMRefreshingTableViewControllerDelegate` protocol.)
+ */
 @property (weak, nonatomic) id <AMRefreshingTableViewControllerDelegate> delegate;
 
+/**
+ *  The number of list items to display per page.
+ */
 @property (assign, nonatomic) NSUInteger listItemsPerPage;
 
+/**
+ *  The array of list items to display. This array is retrieved from the `dataSource`
+ *
+ *  @note Each list item must implement the `AMRefreshingListItemProtocol` in order to display any data on the table view.
+ */
+@property (strong, nonatomic) NSMutableArray *listItemsArray;
+
+/**
+ *  The last page number that was loaded by the view controller. This is used to keep track of the `offset` to request when loading a new page of items from the data source.
+ */
+@property (assign, nonatomic) NSUInteger lastPageLoaded;
+
+/**
+ *  The `ALTableViewCellFactory` used to create and configure the table view cells for the controller.
+ */
 @property (strong, nonatomic) ALTableViewCellFactory *cellFactory;
 
 @end
